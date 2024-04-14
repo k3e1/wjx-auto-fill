532a3391c9aa96291cee60b64643351b35e8d5d5
mianyang <i@mareep.net>
2024-04-14 20:10:47 +0800

    异常抛出

--- main.py
+++ main.py
@@ -33,115 +33,128 @@ option.add_experimental_option('useAutomationExtension', False)
 
 if __name__ == "__main__":
 
-    # 创建一个列表来存储已经选择的 IP 地址
-    selected_ips = []
+    # 一次性获取所有的 IP 地址
+    iptext = requests.get(api).text
+    ips = iptext.strip().splitlines()
+    # 确保 ips 的数量小于 epochs
+    epochs = min(epochs, len(ips))
+    # 将所有IP放入一个集合中
+    available_ips = set(ips)
+    # 创建一个集合来存储已经选择的 IP 地址
+    selected_ips = set()
+    # 失败次数计数
+    failed_count = 0
 
-    # 迭代 range(epochs)，同时获取索引和对应的值
-    # TODO 去除index
-    for index, epoch in enumerate(range(epochs)):
-        # 通过 API 链接爬取 IP，这里根据自己的情况进行修改
-        iptext = requests.get(api).text
-        # 按行拆分文本
-        ips = iptext.strip().splitlines()
+    # 迭代 range(epochs)
+    for epoch in range(epochs):
+        try:
+            # 从可用的 IP 地址集合中随机选择一个 IP
+            ip = random.choice(list(available_ips - selected_ips))
+            # 将选择的 IP 添加到已选择的集合中
+            selected_ips.add(ip)
+            # 从可用的 IP 地址中移除已选择的 IP
+            available_ips.remove(ip)
 
-        # 从剩余的 IP 地址中随机选择一个 IP
-        ip = random.choice([ip for ip in ips if ip not in selected_ips])
-        
-        # 将选择的 IP 添加到已选择的列表中
-        selected_ips.append(ip)
-        
-        option.add_argument('--proxy-server={}'.format(ip))
+            option.add_argument('--proxy-server={}'.format(ip))
 
-        driver = webdriver.Edge(options=option)
+            driver = webdriver.Edge(options=option)
 
-        # 修改User-Agent
-        num = random.randint(0, 2)
-        driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": UA[num]})
-        # 将webdriver属性置为undefined
-        driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
-                               {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
+            # 修改User-Agent
+            num = random.randint(0, 2)
+            driver.execute_cdp_cmd("Network.setUserAgentOverride", {"userAgent": UA[num]})
+            # 将webdriver属性置为undefined
+            driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
+                                {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'})
 
-        driver.get(url)
-        
-        # 题号
-        index = 1
-        # 获取题目数量
-        questions = driver.find_elements(By.CLASS_NAME, "field.ui-field-contain")
-        for i in range(1, len(questions) + 1):
-            xpath = '//*[@id="div{}"]'.format(i)
-            question = driver.find_element(By.XPATH, xpath)
-            # 获取题目类型
-            flag = question.get_attribute("type")
-            if flag == '2':
-                index = utils.fill_blank(driver, i, answerList, index)
-                time.sleep(1)
-            elif flag == '3':
-                index = utils.single_choice(driver, i, prob, index)
-                time.sleep(1)
-            elif flag == '4':
-                index = utils.multi_choice(driver, i, prob, index)
-                time.sleep(1)
-            elif flag == '5':
-                index = utils.single_scale(driver, i, prob, index)
-                time.sleep(1)
-            elif flag == '6':
-                xpath = '//*[@id="div{}"]/div[1]/div[2]/span'.format(i)
-                if driver.find_element(By.XPATH, xpath).text.find("【") != -1:
-                    index = utils.multi_matrix_scale(driver, i, prob, index, num)
+            driver.get(url)
+            
+            # 题号
+            index = 1
+            # 获取题目数量
+            questions = driver.find_elements(By.CLASS_NAME, "field.ui-field-contain")
+            for i in range(1, len(questions) + 1):
+                xpath = '//*[@id="div{}"]'.format(i)
+                question = driver.find_element(By.XPATH, xpath)
+                # 获取题目类型
+                flag = question.get_attribute("type")
+                if flag == '2':
+                    index = utils.fill_blank(driver, i, answerList, index)
+                    time.sleep(1)
+                elif flag == '3':
+                    index = utils.single_choice(driver, i, prob, index)
+                    time.sleep(1)
+                elif flag == '4':
+                    index = utils.multi_choice(driver, i, prob, index)
+                    time.sleep(1)
+                elif flag == '5':
+                    index = utils.single_scale(driver, i, prob, index)
+                    time.sleep(1)
+                elif flag == '6':
+                    xpath = '//*[@id="div{}"]/div[1]/div[2]/span'.format(i)
+                    if driver.find_element(By.XPATH, xpath).text.find("【") != -1:
+                        index = utils.multi_matrix_scale(driver, i, prob, index, num)
+                        time.sleep(1)
+                    else:
+                        index = utils.single_matrix_scale(driver, i, prob, index, num)
+                        time.sleep(1)
+                elif flag == '7':
+                    index = utils.select(driver, i, prob, index)
+                    time.sleep(1)
+                elif flag == '8':
+                    index = utils.single_slide(driver, i, prob, index)
+                    time.sleep(2)
+                elif flag == '11':
+                    index = utils.sort(driver, i, prob, index)
+                    time.sleep(1)
+                elif flag == '12':
+                    index = utils.multi_slide(driver, i, prob, index)
                     time.sleep(1)
                 else:
-                    index = utils.single_matrix_scale(driver, i, prob, index, num)
-                    time.sleep(1)
-            elif flag == '7':
-                index = utils.select(driver, i, prob, index)
-                time.sleep(1)
-            elif flag == '8':
-                index = utils.single_slide(driver, i, prob, index)
-                time.sleep(2)
-            elif flag == '11':
-                index = utils.sort(driver, i, prob, index)
-                time.sleep(1)
-            elif flag == '12':
-                index = utils.multi_slide(driver, i, prob, index)
-                time.sleep(1)
-            else:
-                print("没有该题型")
-                sys.exit(0)
-                
-        time.sleep(1)
-        submit_button = driver.find_element(By.XPATH, '//*[@id="ctlNext"]')
-        submit_button.click()
-        time.sleep(1)
-
-        # 请点击智能验证码进行验证！
-        try:
-            comfirm = driver.find_element(By.XPATH, '//*[@id="layui-layer1"]/div[3]/a')
-            comfirm.click()
+                    print("没有该题型")
+                    sys.exit(0)
+                    
             time.sleep(1)
-        except Exception as e:
-            print(e)
+            submit_button = driver.find_element(By.XPATH, '//*[@id="ctlNext"]')
+            submit_button.click()
+            time.sleep(1)
 
-        # 点击按钮开始智能验证
-        try:
-            button = driver.find_element(By.XPATH, '//*[@id="SM_BTN_WRAPPER_1"]')
-            button.click()
-            time.sleep(0.5)
-        except Exception as e:
-            print(e)
-
-        # 滑块验证
-        try:
-            slider = driver.find_element(By.XPATH, '//*[@id="nc_1__scale_text"]/span')
-            time.sleep(0.3)
-            if str(slider.text).startswith("请按住滑块，拖动到最右边"):
-                width = slider.size.get('width')
-                ActionChains(driver).drag_and_drop_by_offset(slider, width, 0).perform()
+            # 请点击智能验证码进行验证！
+            try:
+                comfirm = driver.find_element(By.XPATH, '//*[@id="layui-layer1"]/div[3]/a')
+                comfirm.click()
                 time.sleep(1)
-        except Exception as e:
-            print(e)
+            except Exception as e:
+                print(e)
 
-        time.sleep(3)
-        driver.quit()
-        print("已完成{}份".format(epoch))
+            # 点击按钮开始智能验证
+            try:
+                button = driver.find_element(By.XPATH, '//*[@id="SM_BTN_WRAPPER_1"]')
+                button.click()
+                time.sleep(0.5)
+            except Exception as e:
+                print(e)
+
+            # 滑块验证
+            try:
+                slider = driver.find_element(By.XPATH, '//*[@id="nc_1__scale_text"]/span')
+                time.sleep(0.3)
+                if str(slider.text).startswith("请按住滑块，拖动到最右边"):
+                    width = slider.size.get('width')
+                    ActionChains(driver).drag_and_drop_by_offset(slider, width, 0).perform()
+                    time.sleep(1)
+            except Exception as e:
+                print(e)
+
+            time.sleep(3)
+        except Exception as e:
+            # 如果出现异常，递增失败计数
+            print(f"任务在第 {epoch + 1} 轮失败: {e}")
+            failed_count += 1
+        finally:
+            # 确保在每次迭代后都关闭 driver
+            driver.quit()
+            print(f"已完成 {epoch + 1} 份")
         
     print("全部完成{}份填写".format(epochs))
+    # 最后输出失败的份数
+    print(f"失败 {failed_count} 份")
